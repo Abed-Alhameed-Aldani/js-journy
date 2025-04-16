5c64f7e5fb009ad5e1a3318b63564a8eb3bd9727
Abed-Aldani <abedaldani2@gmail.com>
2025-04-16 23:36:07 +0300

    Adding the given code from Taha

---
+++ logic.js
@@ -0,0 +1,115 @@
+function generateReports(students) {
+
+    return students.map((student) => {
+
+
+      const total = student.scores.reduce((sum, score) => sum + score, 0);
+      const average = Math.round(total / student.scores.length);
+  
+
+      let grade;
+      if (average >= 90) grade = "A";
+
+      else if (average >= 80) grade = "B";
+
+      else if (average >= 70) grade = "C";
+
+
+      else if (average >= 60) grade = "D";
+
+      else grade = "F";
+  
+ 
+      
+      return {
+        name: student.name,
+        average: average,
+        grade: grade,
+      };
+    });
+  }
+
+  
+
+  const students = [
+    { name: "Sara", scores: [90, 85, 92] },
+    { name: "Taha", scores: [70, 68, 72] },
+    { name: "Abed", scores: [100, 100, 100] },
+  ];
+  
+  console.log(generateReports(students));
+  
+
+
+// #QUESTION 2
+
+
+class BankAccount {
+    constructor(ownerName, initialBalance) {
+      this.ownerName = ownerName;
+
+      this.balance = initialBalance;
+    }
+  
+    deposit(amount) {
+      this.balance += amount;
+    }
+  
+    withdraw(amount) {
+      if (amount <= this.balance) {
+        this.balance -= amount;
+      } else {
+        console.log(" Insuficient balance");
+      }
+    }
+  
+    transferTo(anotherAccount, amount) {
+      if (amount <= this.balance) {
+        this.withdraw(amount);
+        anotherAccount.deposit(amount);
+      } else {
+        console.log("Transfer failed: insuficient funds");
+      }
+    }
+  
+    getSummary() {
+      return `${this.ownerName}'s balance is $${this.balance}`;
+    }
+  }
+  
+
+  const acc1 = new BankAccount("abed", 700);
+  const acc2 = new BankAccount("ahmad", 300);
+  
+  acc1.transferTo(acc2, 200);
+  
+  console.log(acc1.getSummary()); 
+  console.log(acc2.getSummary()); 
+  
+  //===============================================================================
+
+  //Question 3:
+  // Question 3: Dynamic To-Do List
+
+// Get elements from the DOM
+const input = document.querySelector(".task-input");
+const button = document.querySelector(".add-btn");
+const taskList = document.querySelector(".task-list");
+
+// When the button is clicked
+button.addEventListener("click", () => {
+  const task = input.value.trim(); // Get the text and remove extra spaces
+
+  if (task !== "") {
+    // Create a new <li> element
+    const listItem = document.createElement("li");
+    listItem.textContent = task;
+
+    // Add it to the list
+    taskList.appendChild(listItem);
+
+    // Clear the input
+    input.value = "";
+  }
+});
+  
