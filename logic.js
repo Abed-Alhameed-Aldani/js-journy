120f8b22d116bca2ec7da02ddb0a91fba542f170
Abed-Aldani <abedaldani2@gmail.com>
2025-04-16 23:42:39 +0300

    Making 2 accounts, and transfering from acc1 to acc2

--- logic.js
+++ logic.js
@@ -30,3 +30,62 @@ function generateReports(students) {
   }
 
   
+
+  const students = [
+    { name: "Sara", scores: [90, 85, 92] },
+    { name: "Taha", scores: [70, 68, 72] },
+    { name: "Abed", scores: [100, 100, 100] },
+  ];
+  
+  console.log(generateReports(students));
+  
+
+
+// #QUESTION 2
+
+
+class BankAccount {
+    constructor(ownerName, initialBalance) {
+      this.ownerName = ownerName;
+
+      this.balance = initialBalance;
+    }
+  
+    deposit(amount) {
+      this.balance += amount;
+    }
+  
+    withdraw(amount) {
+      if (amount <= this.balance) {
+        this.balance -= amount;
+      } else {
+        console.log(" Insuficient balance");
+      }
+    }
+  
+    transferTo(anotherAccount, amount) {
+      if (amount <= this.balance) {
+        this.withdraw(amount);
+        anotherAccount.deposit(amount);
+      } else {
+        console.log("Transfer failed: insuficient funds");
+      }
+    }
+  
+    getSummary() {
+      return `${this.ownerName}'s balance is $${this.balance}`;
+    }
+  }
+  
+
+  const acc1 = new BankAccount("abed", 700);
+  const acc2 = new BankAccount("ahmad", 300);
+  
+  acc1.transferTo(acc2, 200);
+  
+  console.log(acc1.getSummary()); 
+  console.log(acc2.getSummary()); 
+  
+  //===============================================================================
+
+  
